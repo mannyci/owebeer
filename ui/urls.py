e2c87e033e84cef22b82069c4d3fbe2888853f3a
Manas Maiti <manasheart@gmail.com>
2019-04-06 02:35:16 +0530

    .

--- ui/urls.py
+++ ui/urls.py
@@ -4,4 +4,5 @@ from .views import overview, SetupView, DashboardView
 
 urlpatterns = [
     url(r'^setup', view=SetupView.as_view(), name='setup'),
+    url(r'^overview', view=DashboardView.as_view(), name='overview'),
 ]
