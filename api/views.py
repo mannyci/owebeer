328e47ad1e10012f0ce6970f22b3025626c6cd00
mannyci <manasheart@gmail.com>
2019-06-13 18:18:24 +0200

    import

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
+from account.models import Account
+from core.models import Beer
 # Create your views here.
